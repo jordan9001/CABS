04e1f83f8d2e3e3d295949c19462dc274001f9dd
jordan9001 <jordan9001@gmail.com>
2015-05-09 13:31:04 -0600

    Expanded Admin Page

--- admin_tools/cabs_admin/urls.py
+++ admin_tools/cabs_admin/urls.py
@@ -4,4 +4,6 @@ from cabs_admin import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^machines/$', views.machinesPage, name='machinesPage'),
+    url(r'^pools/$', views.poolsPage, name='poolsPage'),
 )
--- admin_tools/cabs_admin/views.py
+++ admin_tools/cabs_admin/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 from cabs_admin.models import Machines
-
+from cabs_admin.models import Pools
 
 
 def index(request):	
-	context = {}
-	return render(request, 'cabs_admin/index.html', context)
+    context = {}
+    return render(request, 'cabs_admin/index.html', context)
+
+def machinesPage(request):
+    machine_list = Machines.objects.using('cabs').all()
+    context = {'section_name': 'Machines', 'machine_list': machine_list}
+    return render(request, 'cabs_admin/machines.html', context)
+
+def poolsPage(request):
+    pool_list = Pools.objects.using('cabs').all()
+    context = {'section_name': 'Pools', 'pool_list' : pool_list}
+    return render(request, 'cabs_admin/pools.html', context)
