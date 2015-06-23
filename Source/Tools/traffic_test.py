6de561cb18d8ec70e43f91a3767ccd9b63525c5b
jordan9001 <jordan9001@gmail.com>
2015-06-23 15:56:02 -0600

    Made traffic_test Binaries

--- Source/Tools/traffic_test.py
+++ Source/Tools/traffic_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import socket, ssl
 import os
+import struct
 import random
 import sys
 import string
@@ -25,10 +26,14 @@ def splash():
 def hitServer():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((settings.get("Host"), settings.get("Port")))
+        s.connect((settings.get("Host"), int(settings.get("Port"))))
     except Exception as e:
         print "Error:", e
     else:
+        if settings.get("Evil_Close") == 'True':
+            linger = 1
+            lingertime = 0
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_LINGER, struct.pack('ii', linger, lingertime))
         if not settings.get("Message"):
             name = "".join( [random.choice(string.letters) for i in range(int(settings.get("Rand_Length")))] )
             paswd = "".join( [random.choice(string.letters) for i in range(int(settings.get("Rand_Length")))] )
@@ -45,12 +50,13 @@ def hitServer():
         s_wrapped.close()
 
 def printUsage(extend=False):
-    print "Usage : "+sys.argv[0]+" -h host [-p port] [-v] [-s /path/to/cert.pem] [-r] [-n (#tries | loop)] [-t #threads] [(-m msg) | (-l msg_length)]"
+    print "Usage : "+sys.argv[0]+" -h host [-p port] [-v] [-s /path/to/cert.pem] [-r] [-e] [-n (#tries | loop)] [-t #threads] [(-m msg) | (-l msg_length)]"
     if extend:
         print "  -h host           : the CABS Broker Server"
         print "  -p port           : the CABS Broker Port for Clients or Agents"
         print "  -s /to/cert.pem   : the path to the CABS Broker certificate.pem"
         print "  -r                : don't add a valid return at the end of the message"
+        print "  -e                : disconnect evily, with no FIN packet"
         print "  -n (#tries | loop): the number of requests to send, or loop until Ctrl-C"
         print "  -t #threads       : the number of threads for making requests"
         print "  -m msg            : the message to send, otherwise a random user name and password will be sent in a pool request"
@@ -82,6 +88,8 @@ def getArgs():
                 option = "SSL_Cert"
             elif sys.argv[i] in ['-r', '--return', '-return']:
                 settings["No_Return"] = 'True'
+            elif sys.argv[i] in ['-e', '--evil', '-evil']:
+                settings["Evil_Close"] = 'True'
             elif sys.argv[i] in ['-m', '--msg', '-msg', '--message', '-message']:
                 option = "Message"
             elif sys.argv[i] in ['-l', '--length', '-length', '--msg_length']:
@@ -95,7 +103,7 @@ def getArgs():
     if not settings.get("Host"):
         printUsage()
     if not settings.get("Port"):
-        settings["Port"] = 18181
+        settings["Port"] = "18181"
     if not settings.get("Rand_Length"):
         settings["Rand_Length"] = "30"
     if not settings.get("Number"):
@@ -124,7 +132,7 @@ def gucci_main():
             threads.append(t)
             t.start()
         if settings.get("Number") == 'loop':
-            input("Running... Press Enter to end.")
+            input("Running... Press Enter to end")
     else:
         runHit()
     
